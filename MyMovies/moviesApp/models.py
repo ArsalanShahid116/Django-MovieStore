b1e4c00f8d52ea85dce95ebe674a4607d12a7443
arsalan <arsalan.shahid@ucdconnect.ie>
2019-06-10 00:10:46 +0100

    Minor corrections: Blog Post - MovieStore Part 5

--- MyMovies/moviesApp/models.py
+++ MyMovies/moviesApp/models.py
@@ -79,8 +79,8 @@ class Vote(models.Model):
     UP = 1
     DOWN = -1
     VALUE_CHOICES = (
-            (UP, "ðŸ‘",),
-            (DOWN, "ðŸ‘Ž",),
+            (UP, "LIKE",),
+            (DOWN, "DISLIKE",),
             )
 
     value = models.SmallIntegerField(
@@ -102,15 +102,3 @@ class Vote(models.Model):
 
     class Meta:
         unique_together = ('user', 'movie')
-
-class VoteManager(models.Manager):
-    def get_vote_or_unsaved_blank_vote(self, movie, user):
-        try:
-            return Vote.objects.get(
-                movie=movie,
-                user=user)
-        except Vote.DoesNotExist:
-            return Vote(
-                movie=movie,
-                user=user)
-
